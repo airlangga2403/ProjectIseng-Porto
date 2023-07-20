b53a684ee5483aa4cf01cd32dabdc3e0170ce03d
Zeeus <airlanggapermana96@gmail.com>
2023-07-21 01:38:26 +0700

    Implement Onboarding Using Motion Layout

--- android/1. Bress Mobile App/app/src/main/java/com/example/breesmobileapp/ui/SplashScreenActivity.kt
+++ android/1. Bress Mobile App/app/src/main/java/com/example/breesmobileapp/ui/SplashScreenActivity.kt
@@ -1,6 +1,7 @@
 package com.example.breesmobileapp.ui
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +10,7 @@ import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import com.example.breesmobileapp.R
 import com.example.breesmobileapp.databinding.ActivitySplashScreenBinding
+import com.example.breesmobileapp.ui.onboarding.OnboardingActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -19,10 +21,20 @@ class SplashScreenActivity : AppCompatActivity() {
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val ivSmoke = findViewById<ImageView>(R.id.iv_smoke)
-        val backgroundDrawable = ivSmoke.background
-        Log.d("Animation", "ImageView Background: $backgroundDrawable")
-
+        // Delay the splash screen for 2 seconds (adjust as needed)
+        val splashDurationMillis = 2000
+        Thread {
+            try {
+                Thread.sleep(splashDurationMillis.toLong())
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            } finally {
+                // After the splash screen duration, start the next activity
+                val intent = Intent(this, OnboardingActivity::class.java) // Replace MainActivity with your desired activity
+                startActivity(intent)
+                finish() // Optional: finish the splash screen activity so it won't be shown when back button is pressed
+            }
+        }.start()
 
     }
 }
