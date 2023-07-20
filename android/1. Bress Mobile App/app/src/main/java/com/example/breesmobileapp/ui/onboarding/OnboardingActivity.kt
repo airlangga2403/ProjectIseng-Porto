354d1d1d13edf3a5ff871e4329189ee52c8ddbd8
Zeeus <airlanggapermana96@gmail.com>
2023-07-21 02:18:23 +0700

    Login UI

--- android/1. Bress Mobile App/app/src/main/java/com/example/breesmobileapp/ui/onboarding/OnboardingActivity.kt
+++ android/1. Bress Mobile App/app/src/main/java/com/example/breesmobileapp/ui/onboarding/OnboardingActivity.kt
@@ -1,9 +1,11 @@
 package com.example.breesmobileapp.ui.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.breesmobileapp.R
 import com.example.breesmobileapp.databinding.ActivityOnboardingBinding
+import com.example.breesmobileapp.ui.login.LoginActivity
 
 class OnboardingActivity : AppCompatActivity() {
 
@@ -13,5 +15,10 @@ class OnboardingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityOnboardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnOb3.setOnClickListener {
+            startActivity(Intent(this@OnboardingActivity, LoginActivity::class.java))
+        }
+
     }
 }
